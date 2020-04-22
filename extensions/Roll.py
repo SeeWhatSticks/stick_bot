2ffaf987d42cdf2f4bd07c80667c939b2ff2dfe8
Edward Jones <mail@erj4.uk>
2020-04-22 20:36:49 +0100

    Fix roll

--- extensions/Roll.py
+++ extensions/Roll.py
@@ -5,8 +5,10 @@ from discord.ext import commands
 
 COLOR = 0xff9933
 
+
 class Roll(commands.Cog):
     """Introduces some different kinds of random chance."""
+
     def __init__(self, bot):
         self.bot = bot
 
@@ -14,16 +16,19 @@ class Roll(commands.Cog):
     async def flip(self, ctx):
         """Flip a coin."""
         await ctx.channel.send(embed=Embed(
-                title="Flipped a coin for {}:".format(ctx.author.display_name),
-                description="It came up {}!".format(choice(["heads", "tails"])),
-                color=COLOR))
+            title="Flipped a coin for {}:".format(ctx.author.display_name),
+            description="It came up {}!".format(choice(["heads", "tails"])),
+            color=COLOR))
 
     @commands.command()
-    async def roll(self, ctx):  #, input_str: typing.Optional[str]):
+    # , input_str: typing.Optional[str]):
+    async def roll(self, ctx, maximum: int = 6):
         """Roll some dice."""
         await ctx.channel.send(embed=Embed(
-            title="Rolled a six-sider for {}:".format(ctx.author.display_name),
-            description="It was a {}!".format(randint(6)),
+            title="Rolled a {}-sider for {}:".format(
+                maximum, ctx.author.display_name
+            ),
+            description="It was a {}!".format(randint(1, maximum)),
             color=COLOR))
         # bits = []
         # while any([v in input_str for v in ["+", "-"]]):
@@ -34,11 +39,12 @@ class Roll(commands.Cog):
         """Choose from a list."""
         options = input_str.split(sep="/")
         await ctx.channel.send(embed=Embed(
-                title="Made a choice for {}:".format(ctx.author.display_name),
-                description="From {} options: {}".format(
-                        len(options),
-                        choice(options)),
-                color=COLOR))
+            title="Made a choice for {}:".format(ctx.author.display_name),
+            description="From {} options: {}".format(
+                len(options),
+                choice(options)),
+            color=COLOR))
+
 
 def setup(bot):
     bot.add_cog(Roll(bot))
